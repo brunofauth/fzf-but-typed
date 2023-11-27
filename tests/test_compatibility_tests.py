8f1bbb36e053a6afd945c70d1fa8d95059d402bb
Bruno Fauth <149593@upf.br>
2023-11-26 22:37:10 -0300

    improved fzf-version compatibility-testing; and added pytest-testing to said test

---
+++ tests/test_compatibility_tests.py
@@ -0,0 +1,39 @@
+from fzf_but_typed.compatibility import SemVer, _test_compatibility
+import pytest
+import functools
+
+
+def test_compatibility_test() -> None:
+    mock_all_supported_versions = [
+        SemVer(major=0, minor=1, patch=0),
+        SemVer(major=0, minor=1, patch=1),
+        SemVer(major=0, minor=3, patch=0),
+        SemVer(major=1, minor=3, patch=1),
+    ]
+
+    test1 = functools.partial(
+        _test_compatibility,
+        latest_supported=mock_all_supported_versions[-1],
+        all_supported_versions=mock_all_supported_versions,
+    )
+
+    with pytest.raises(RuntimeError, match="which is newer"):
+        test1(found_version=SemVer(major=2, minor=0, patch=0))
+
+    with pytest.raises(RuntimeError, match="which is older"):
+        test1(found_version=SemVer(major=0, minor=0, patch=0))
+
+    with pytest.raises(RuntimeError, match="which is older"):
+        test1(found_version=SemVer(major=0, minor=2, patch=0))
+
+    with pytest.raises(RuntimeError, match="has been supported by a previous"):
+        test1(found_version=SemVer(major=0, minor=3, patch=0))
+
+    with pytest.raises(SystemExit):
+        test1(found_version=SemVer(major=1, minor=3, patch=0))
+
+    with pytest.raises(SystemExit):
+        test1(found_version=SemVer(major=1, minor=3, patch=1))
+
+    with pytest.raises(SystemExit):
+        test1(found_version=SemVer(major=1, minor=3, patch=2))
